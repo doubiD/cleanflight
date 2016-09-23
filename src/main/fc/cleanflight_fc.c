ad6c47d3657fbbc9a9eb620f565487abb7e2d1e2
Hydra <dominic.clifton@cleanflight.com>
2016-09-23 21:26:00 +0200

    Minor changes to SKIP/USE and flash size defines.
    To avoid double negatives we prefer `#ifdef USE_*` instead of `#ifndef
    SKIP_*`
    
    The CJMCU target still won't fit.

--- src/main/fc/cleanflight_fc.c
+++ src/main/fc/cleanflight_fc.c
@@ -21,7 +21,9 @@
 #include <math.h>
 
 #include <platform.h>
+
 #include "build/debug.h"
+#include "build/build_config.h"
 
 #include "common/maths.h"
 #include "common/axis.h"
@@ -219,7 +221,7 @@ static void updateRcCommands(void)
             // YAW TPA disabled.
             PIDweight[axis] = 100;
         }
-#ifndef SKIP_PID_MW23
+#ifdef USE_PID_MW23
         // FIXME axis indexes into pids.  use something like lookupPidIndex(rc_alias_e alias) to reduce coupling.
         dynP8[axis] = (uint16_t)pidProfile()->P8[axis] * prop1 / 100;
         dynI8[axis] = (uint16_t)pidProfile()->I8[axis] * prop1 / 100;
@@ -456,7 +458,7 @@ void processRx(void)
                 DISABLE_STATE(ANTI_WINDUP);
             }
         } else {
-#ifndef SKIP_PID_MW23
+#ifdef USE_PID_MW23
             pidResetITermAngle();
 #endif
             pidResetITerm();
@@ -529,7 +531,7 @@ void processRx(void)
         canUseHorizonMode = false;
 
         if (!FLIGHT_MODE(ANGLE_MODE)) {
-#ifndef SKIP_PID_MW23
+#ifdef USE_PID_MW23
             pidResetITermAngle();
 #endif
             ENABLE_FLIGHT_MODE(ANGLE_MODE);
@@ -543,7 +545,7 @@ void processRx(void)
         DISABLE_FLIGHT_MODE(ANGLE_MODE);
 
         if (!FLIGHT_MODE(HORIZON_MODE)) {
-#ifndef SKIP_PID_MW23
+#ifdef USE_PID_MW23
             pidResetITermAngle();
 #endif
             ENABLE_FLIGHT_MODE(HORIZON_MODE);
--- src/main/io/serial_cli.c
+++ src/main/io/serial_cli.c
@@ -134,7 +134,10 @@ static void cliDump(char *cmdLine);
 static void cliExit(char *cmdline);
 static void cliFeature(char *cmdline);
 static void cliMotor(char *cmdline);
+#ifdef BUZZER
 static void cliPlaySound(char *cmdline);
+#endif
+
 static void cliProfile(char *cmdline);
 static void cliRateProfile(char *cmdline);
 static void cliReboot(void);
@@ -301,8 +304,10 @@ const clicmd_t cmdTable[] = {
     CLI_COMMAND_DEF("mmix", "custom motor mixer", NULL, cliMotorMix),
     CLI_COMMAND_DEF("motor",  "get/set motor",
        "<index> [<value>]", cliMotor),
+#ifdef BUZZER
     CLI_COMMAND_DEF("play_sound", NULL,
         "[<index>]\r\n", cliPlaySound),
+#endif
     CLI_COMMAND_DEF("profile", "change profile",
         "[<index>]", cliProfile),
     CLI_COMMAND_DEF("rateprofile", "change rate profile",
@@ -368,9 +373,11 @@ static const char * const lookupTableAmperageMeter[] = {
     "VIRTUAL", "ADC"
 };
 
+#ifdef USE_SERVOS
 static const char * const lookupTableGimbalMode[] = {
     "NORMAL", "MIXTILT"
 };
+#endif
 
 static const char * const lookupTablePidController[] = {
     "MW23", "MWREWRITE", "LUX"
@@ -438,7 +445,9 @@ typedef enum {
     TABLE_BLACKBOX_DEVICE,
 #endif
     TABLE_AMPERAGE_METER,
+#ifdef USE_SERVOS
     TABLE_GIMBAL_MODE,
+#endif
     TABLE_PID_CONTROLLER,
     TABLE_SERIAL_RX,
     TABLE_GYRO_FILTER,
@@ -460,7 +469,9 @@ static const lookupTableEntry_t lookupTables[] = {
     { lookupTableBlackboxDevice, sizeof(lookupTableBlackboxDevice) / sizeof(char *) },
 #endif
     { lookupTableAmperageMeter, sizeof(lookupTableAmperageMeter) / sizeof(char *) },
+#ifdef USE_SERVOS
     { lookupTableGimbalMode, sizeof(lookupTableGimbalMode) / sizeof(char *) },
+#endif
     { lookupTablePidController, sizeof(lookupTablePidController) / sizeof(char *) },
     { lookupTableSerialRX, sizeof(lookupTableSerialRX) / sizeof(char *) },
     { lookupTableGyroFilter, sizeof(lookupTableGyroFilter) / sizeof(char *) },
@@ -2167,11 +2178,9 @@ static void cliMotor(char *cmdline)
     cliPrintf("motor %d: %d\r\n", motor_index, motor_disarmed[motor_index]);
 }
 
+#ifdef BUZZER
 static void cliPlaySound(char *cmdline)
 {
-#if FLASH_SIZE <= 64
-    UNUSED(cmdline);
-#else
     int i;
     const char *name;
     static int lastSoundIdx = -1;
@@ -2201,8 +2210,8 @@ static void cliPlaySound(char *cmdline)
     beeperSilence();
     cliPrintf("Playing sound %d: %s\r\n", i, name);
     beeper(beeperModeForTableIndex(i));
-#endif
 }
+#endif
 
 static void cliProfile(char *cmdline)
 {
